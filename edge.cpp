597e2291fc8d9a97b8530cb244923c96a1388f6a
BrycePy <su287@bath.ac.uk>
2022-04-20 12:27:24 +0100

    reordered work dir

--- edge.cpp
+++ edge.cpp
@@ -9,6 +9,7 @@ using namespace cv;
 
 Mat kernel = getStructuringElement(MORPH_RECT, Size(7, 7));
 Mat avgImage(Size(400, 400), CV_8UC3, Scalar(0));
+Mat kernelLarge = getStructuringElement(MORPH_RECT, Size(51, 51));
 
 int findTL(vector<Point> p) {
     int min = 0;
@@ -22,8 +23,12 @@ int findTL(vector<Point> p) {
 
 Mat getEdge(Mat image, int blurSize, int thresholdValue) {
     Mat imageValue, imageValueNoBlur, imageEdge;
-    cvtColor(image, imageValue, COLOR_BGR2HSV);
-    extractChannel(imageValue, imageValue, 2);
+    if(image.channels() == 3) {
+        cvtColor(image, imageValue, COLOR_BGR2HSV);
+        extractChannel(imageValue, imageValue, 2);
+    } else {
+        imageValue = image.clone();
+    }
     imageValueNoBlur = imageValue.clone();
     GaussianBlur(imageValue, imageValue, Size(blurSize, blurSize), 5, 0);
     absdiff(imageValueNoBlur, imageValue, imageEdge);
@@ -32,12 +37,14 @@ Mat getEdge(Mat image, int blurSize, int thresholdValue) {
 }
 
 Mat processFeed(Mat image) {
+    // ===== generate edge image =====
     // Mat imageEdge = getEdge(image, 5, 5);
     Mat imageEdge;
     cvtColor(image, imageEdge, COLOR_BGR2GRAY);
     Canny(imageEdge, imageEdge, 25, 200, 3);
     imshow("imageEdge", imageEdge);
 
+    // ===== find contours =====
     vector<vector<Point>> contours, scratch;
     vector<Vec4i> hierarchy;
     findContours(imageEdge, contours, hierarchy, RETR_EXTERNAL,
@@ -46,6 +53,7 @@ Mat processFeed(Mat image) {
     for (int i = 0; i < contours.size(); i++)
         approxPolyDP(contours[i], conPoly[i], 30, true);
 
+    // ===== find largest rect contour =====
     float max_area = 10000;
     int max_rect = 0;
 
@@ -60,7 +68,7 @@ Mat processFeed(Mat image) {
     }
 
     Mat imageCont = image.clone();
-    Mat imageWarp, imageWarpRGB;
+    Mat imageWarpHSV, imageWarpRGB, imageWarpGray;
 
     int size = 400;
     if (max_area > 10000) {
@@ -73,47 +81,44 @@ Mat processFeed(Mat image) {
                           {0.0, (float)size},
                           {(float)size, (float)size}};
 
-        Mat matric = getPerspectiveTransform(src, dst);
-        Mat invMatric = getPerspectiveTransform(dst, src);
+        Mat warpMatric = getPerspectiveTransform(src, dst);
+        Mat invWarpMatric = getPerspectiveTransform(dst, src);
         drawContours(imageCont, conPoly, max_rect, Scalar(0, 255, 0), 3);
         for (int i = 0; i < p.size(); i++) {
             circle(imageCont, p[i], 3, Scalar(0, 100, 255), -1);
-            putText(imageCont, to_string(i), p[i], FONT_HERSHEY_SIMPLEX, 1,
-                    Scalar(0, 0, 255), 2);
+            putText(imageCont, to_string(i), p[i], FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
         }
 
-        warpPerspective(image, imageWarpRGB, matric, Size(size, size));
+        warpPerspective(image, imageWarpRGB, warpMatric, Size(size, size));
         imshow("imageWarp", imageWarpRGB);
 
         Rect crop(5, 5, size - 10, size - 10);
         imageWarpRGB = imageWarpRGB(crop);
         resize(imageWarpRGB, imageWarpRGB, Size(size, size));
 
-        vector<Mat> channels;
-        Mat imageFinal;
+        cvtColor(imageWarpRGB, imageWarpHSV, COLOR_BGR2HSV);
+        cvtColor(imageWarpRGB, imageWarpGray, COLOR_BGR2GRAY);
 
         Mat imageScratch = getEdge(imageWarpRGB, 5, 3);
-        cvtColor(imageWarpRGB, imageWarp, COLOR_BGR2HSV);
-        Mat imageOil;
-
         imageScratch *= 3;
         GaussianBlur(imageScratch, imageScratch, Size(31, 31), 0, 0);
         threshold(imageScratch, imageScratch, 10, 255, THRESH_BINARY);
+        findContours(imageScratch, scratch, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-        findContours(imageScratch, scratch, hierarchy, RETR_TREE,
-                     CHAIN_APPROX_SIMPLE);
+        Mat imageOil;
+        GaussianBlur(imageWarpGray, imageWarpGray, Size(15, 15), 0, 0);
+        imageOil = getEdge(imageWarpGray, 15, 5);
+        GaussianBlur(imageOil, imageOil, Size(31, 31), 0, 0);
+        threshold(imageOil, imageOil, 30, 255, THRESH_BINARY);
+        dilate(imageOil, imageOil, kernelLarge);
 
-        extractChannel(imageWarp, imageOil, 1);
-        imageOil *= 3;
-        GaussianBlur(imageOil, imageOil, Size(5, 5), 0, 0);
-        threshold(imageOil, imageOil, 70, 255, THRESH_BINARY);
+        Mat imageBlank = Mat::zeros(Size(imageWarpHSV.rows, imageWarpHSV.cols), CV_8UC1);
 
-        Mat imageS = Mat::zeros(Size(imageWarp.rows, imageWarp.cols), CV_8UC1);
-
-        channels.push_back(imageS);
+        Mat imageFinal;
+        vector<Mat> channels;
         channels.push_back(imageOil);
-        channels.push_back(imageScratch);
-
+        channels.push_back(imageBlank);
+        channels.push_back(imageOil * 0.5);
         merge(channels, imageFinal);
 
         avgImage = avgImage * 0.9 + imageFinal * 0.2;
@@ -131,12 +136,12 @@ Mat processFeed(Mat image) {
         Mat imageFinalOverlay = avgImage * 0.8 + imageWarpRGB;
         imshow("imageFinal", imageFinalOverlay);
 
-        warpPerspective(avgImage, imageFinalOverlay, invMatric, Size(image.cols, image.rows));
+        warpPerspective(avgImage, imageFinalOverlay, invWarpMatric, Size(image.cols, image.rows));
 
         imshow("imageInvWarpFinal", image + imageFinalOverlay);
 
         imshow("imageCont", imageCont);
-        return matric;
+        return warpMatric;
     }
 
     imshow("imageCont", imageCont);
@@ -170,7 +175,7 @@ int main(int, char**) {
         processFeed(camRGB);
         camDebug(camRGB);
 
-        if (waitKey(1) >= 0) {
+        if (waitKey(10) >= 0) {
             break;
         }
 
