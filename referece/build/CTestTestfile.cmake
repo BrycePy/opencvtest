fd0a026a87b074b6e8ef433509cf9e64b2f7f58a
BrycePy <su287@bath.ac.uk>
2022-04-21 10:17:25 +0100

    ignore build files 2

--- referece/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/boomz/Desktop/sandbox/opencvtest
-# Build directory: C:/Users/boomz/Desktop/sandbox/opencvtest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
